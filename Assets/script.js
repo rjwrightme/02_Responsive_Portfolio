bf575e9328ac868768a1009f8b0d8c36405455ba
RJ Wright <rj@rjwright.me>
2020-08-22 17:27:52 +1000

    Add homepage hero image + nav

---
+++ Assets/script.js
@@ -0,0 +1,27 @@
+let i = 0;
+let j = 0;
+let txt = ['full stack development','web design','3D animation','videography','product photography'];
+let speed = 80;
+let heading = document.querySelector('#typewriterText');
+
+function typeWriter() {
+    if (i < txt[j].length) {
+        heading.innerHTML += txt[j].charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    } else {
+        (j < txt.length - 1) ? j++ : j = 0;
+        setTimeout(eraseText, 1200);
+    }
+}
+
+function eraseText() {
+    if (i > 0 ) {
+        heading.innerHTML = heading.innerHTML.slice(0, -1);
+        i = heading.innerHTML.length;
+        setTimeout(eraseText, 30);
+    } else {
+        typeWriter();
+    }
+}
+setTimeout(typeWriter, 1000);
